d84e3c8648e69333f92791f0fb52ef5b6850a904
ifateli <ifateli@post.bgu.ac.il>
2015-06-21 22:03:18 +0300

    new project- ForumApp + ForumTest + unitTesting

---
+++ ForumApplication/Controllers/CreateForumController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using ForumApplication.Models;
+
+namespace ForumApplication.Controllers
+{
+    public class CreateForumController : Controller
+    {
+        public ActionResult CreateForum(string id)
+        {
+            if (ForumApplication.Models.ForumSystem.superadmin.Equals(id))
+                return View();
+            else
+                return View("../OnlySuperAdmin");
+        }
+    
+
+        [HttpGet]
+        [HttpPost]
+        public ActionResult CreateNewForum(string id)
+        {
+            string title = Request["Forumtitle"].ToString();
+            string admin = Request["adminUserName"].ToString();
+            List<string> admins = new List<string>();
+            admins.Add(admin);
+            ForumSystem fs = ForumSystem.initForumSystem();
+            Forum newForum = new Forum(title, admins);
+            bool res = fs.createForum(newForum, id);
+            if (res)
+                return View();
+            else
+                return View("OnlySuperAdmin");
+
+        }
+
+    }
+}
---
+++ unitTestingDevelopers/ForumUT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ForumApplication;
+using ForumApplication.Models;
+namespace unitTestingDevelopers
+{
+    [TestClass]
+    public class ForumUT
+    {
+        ForumSystem system = ForumSystem.initForumSystem();
+        [TestMethod]
+        public void checkMember1()
+        {
+            Member CheckingMember = system.addMember("ifateli", "gilAd", "ifateli@bgu.ac.il");
+            Assert.IsTrue(system.Members.ContainsKey("ifateli"));
+        }
+    }
+}
