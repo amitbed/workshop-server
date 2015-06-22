c6025c333768dc943b1c135c397e3d750d7c1434
Amit Bedarshi <amitbed@post.bgu.ac.il>
2015-06-22 20:12:34 +0300

    add apiAdmincontroller

---
+++ ForumApplication/Controllers/ApiAdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ForumApplication.Controllers
+{
+    public class ApiAdminController : ApiController
+    {
+        //check if user is admin in the forum
+        public bool Get(List<string> args)
+        {
+            fs = ForumSystem.initForumSystem();
+            string username = args.ElementAt(0);
+            string forumTitle = args.ElementAt(1);
+            if (fs.Members.ContainsKey(username))
+            {
+                Member mem = fs.Members[username];
+                return (mem.MyForums.Contains(forumTitle));
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
