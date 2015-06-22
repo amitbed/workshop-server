f6aa2d08c5c1c2b0da4fccbd32d34d41be6b3c52
sagi <asagip8@hotmail.com>
2015-06-22 10:33:15 +0300

    Sub Forums is good

--- ForumApplication/Controllers/CreateSubForumController.cs
+++ ForumApplication/Controllers/CreateSubForumController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ForumApplication.Models;
 
 namespace ForumApplication.Controllers
 {
@@ -16,8 +17,18 @@ namespace ForumApplication.Controllers
             return View();
         }
 
-        public ActionResult CreateNewSubFourm()
+        public ActionResult CreateNewSubForum()
         {
+            string title = Request["SubForumtitle"].ToString();
+            string moderator = Request["moderators"].ToString();
+            string parent = Request["parent"].ToString().ToLower();
+            int maxModerators = Int32.Parse(Request["maxModerators"].ToString());
+            
+            List<string> moderators = new List<string>();
+            moderators.Add(moderator);
+            ForumSystem fs = ForumSystem.initForumSystem();
+            fs.Forums[parent].createSubForum(title,moderators,parent,maxModerators);
+            ViewData["subForumParent"] = parent;
             return View();
         }
     }
